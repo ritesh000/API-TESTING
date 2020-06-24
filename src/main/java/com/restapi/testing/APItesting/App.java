69f4c20efb962a18907182029ac667eb8e743e57
rites <rites@192.168.1.9>
2020-06-24 13:41:35 +0530

    test api

---
+++ src/main/java/com/restapi/testing/APItesting/App.java
@@ -0,0 +1,13 @@
+package com.restapi.testing.APItesting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
